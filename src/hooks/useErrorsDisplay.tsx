929f7ac61f6305b463ab8ddb1bfa6e9e27da4c03
digunin <kandidad@gmail.com>
2024-06-01 19:29:42 +0300

    reset user if crud-actions return 'access deny' error

--- src/hooks/useErrorsDisplay.tsx
+++ src/hooks/useErrorsDisplay.tsx
@@ -2,8 +2,12 @@ import { useEffect } from "react";
 import { useSignIn } from "./useSignIn";
 import { useAppDatabase } from "./useAppDatabase";
 import { useAppSnackbar } from "./useAppSnackbar";
+import { error_messages } from "../utils/text";
+import { useAppDispatch } from "../store";
+import { setUser } from "../store/userSlice";
 
 export const useErrorsDisplay = () => {
+  const dispatch = useAppDispatch();
   const { showSnackbar } = useAppSnackbar();
   const { error: userError } = useSignIn();
   const { error: dataError } = useAppDatabase();
@@ -11,5 +15,7 @@ export const useErrorsDisplay = () => {
   useEffect(() => {
     if (userError) showSnackbar(userError);
     if (dataError) showSnackbar(dataError);
+    if (dataError === error_messages.authFailed)
+      dispatch(setUser({ username: null, token: null }));
   }, [userError, dataError]);
 };
