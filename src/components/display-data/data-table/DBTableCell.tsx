10bde963109e90a15de0dcba80aaa943e9288f24
digunin <kandidad@gmail.com>
2024-06-18 15:34:47 +0300

    table cell

---
+++ src/components/display-data/data-table/DBTableCell.tsx
@@ -0,0 +1,57 @@
+import { Grid, GridProps, TextField, Tooltip } from "@mui/material";
+import React, { FC } from "react";
+import { ColumnType } from "./column-settings";
+
+export type DBCell = GridProps & {
+  editMode?: boolean;
+  value: string;
+  label: string;
+  isHead?: boolean;
+  oneLineRow?: boolean;
+  columnType?: ColumnType;
+};
+
+const DBTableCell: FC<DBCell> = ({
+  value,
+  label,
+  isHead,
+  editMode,
+  oneLineRow,
+  columnType = "text",
+  ...gridProps
+}) => {
+  const toolTip =
+    !isHead && columnType === "date-time"
+      ? new Date(value).toLocaleString()
+      : value;
+  const correctValue =
+    !isHead && columnType === "date-time"
+      ? new Date(value).toLocaleDateString()
+      : value;
+
+  return (
+    <Grid item {...gridProps}>
+      <Tooltip title={`${oneLineRow ? toolTip : ""}`}>
+        <TextField
+          InputProps={{
+            readOnly: true,
+          }}
+          fullWidth
+          variant={isHead ? "filled" : "outlined"}
+          label={oneLineRow ? "" : label}
+          value={correctValue}
+          hiddenLabel={isHead && oneLineRow}
+          margin={oneLineRow ? "none" : "dense"}
+          sx={{
+            "& .MuiInputBase-input": {
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            },
+          }}
+        />
+      </Tooltip>
+    </Grid>
+  );
+};
+
+export default DBTableCell;
