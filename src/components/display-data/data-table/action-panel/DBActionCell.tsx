ae97751753c3f4de586273eb67b15056fcb3cce4
digunin <kandidad@gmail.com>
2024-06-18 15:35:41 +0300

    table action cell

---
+++ src/components/display-data/data-table/action-panel/DBActionCell.tsx
@@ -0,0 +1,48 @@
+import React, { FC } from "react";
+import { Grid } from "@mui/material";
+import { DBCell } from "../DBTableCell";
+import DBActionPanel, {
+  EditModeCallbacks,
+  ViewModeCallbacks,
+} from "./DBActionPanel";
+
+type DBACell = ViewModeCallbacks &
+  EditModeCallbacks &
+  Omit<DBCell, "label" | "value"> & { isLoading?: boolean };
+
+const DBActionCell: FC<DBACell> = ({
+  isHead,
+  onedit,
+  ondelete,
+  oncancel,
+  onsave,
+  editMode,
+  isLoading,
+  ...gridProps
+}) => {
+  return (
+    <Grid item {...gridProps}>
+      {editMode ? (
+        <DBActionPanel<"edit">
+          mode="edit"
+          isLoading={isLoading}
+          callbacks={{
+            oncancel,
+            onsave,
+          }}
+        />
+      ) : (
+        <DBActionPanel<"view">
+          mode="view"
+          isLoading={isLoading}
+          callbacks={{
+            onedit,
+            ondelete,
+          }}
+        />
+      )}
+    </Grid>
+  );
+};
+
+export default DBActionCell;
