2753e722d3d6f2327d6b4475d63f7153aceec228
digunin <kandidad@gmail.com>
2024-06-19 20:29:33 +0300

    pass new props to DBTable

--- src/components/display-data/data-table/DBTable.tsx
+++ src/components/display-data/data-table/DBTable.tsx
@@ -2,39 +2,62 @@ import React, { FC } from "react";
 import DBTableHeader from "./DBTableHeader";
 import DBTableRow from "./DBTableRow";
 import { Breakpoint, useMediaQuery, useTheme } from "@mui/material";
-import { DatabaseData } from "../../../api/types";
+import { DatabaseData, RecordID, SingleRecord } from "../../../api/types";
 import DBTableRowExpandable from "./DBTableRowExpandable";
+import { FormState } from "../../../store/form/types";
 
 type DBTableProps = {
   data: DatabaseData;
   loading: boolean;
   oneLineRowBreakpoint: Breakpoint;
+  isCreate: boolean;
+  isEdit: RecordID | null;
+  inputFields: FormState<"dbrecord">;
+} & DBHandlers;
+
+export type DBHandlers = {
+  onedit: (id: RecordID) => void;
+  ondelete: (id: RecordID) => void;
+  oncancel: () => void;
+  onsave: () => void;
+  oninput: (name: keyof SingleRecord) => (value: string, error: string) => void;
 };
 
 const DatabaseTable: FC<DBTableProps> = ({
   data,
   loading,
   oneLineRowBreakpoint,
+  isCreate,
+  isEdit,
+  inputFields,
+  ...handlers
 }) => {
   const theme = useTheme();
   const oneLineRow = useMediaQuery(theme.breakpoints.up(oneLineRowBreakpoint));
   return (
     <div className="database-table">
       {oneLineRow && (
-        <DBTableHeader oneLineRowBreakpoint={oneLineRowBreakpoint} />
+        <DBTableHeader
+          oneLineRowBreakpoint={oneLineRowBreakpoint}
+          {...handlers}
+        />
       )}
       {data.map((row) =>
         oneLineRow ? (
           <DBTableRow
             key={row.id}
             row={row}
+            inputFields={isEdit === row.id ? inputFields : undefined}
             oneLineRowBreakpoint={oneLineRowBreakpoint}
+            {...handlers}
           />
         ) : (
           <DBTableRowExpandable
             key={row.id}
             row={row}
+            inputFields={isEdit === row.id ? inputFields : undefined}
             oneLineRowBreakpoint={oneLineRowBreakpoint}
+            {...handlers}
           />
         )
       )}
