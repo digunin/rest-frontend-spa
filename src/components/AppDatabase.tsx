9b06639e6bea7f225c8752411143afd87d45d10f
digunin <kandidad@gmail.com>
2024-06-19 20:31:39 +0300

    use 'DBTable' component for data displaying

--- src/components/AppDatabase.tsx
+++ src/components/AppDatabase.tsx
@@ -1,17 +1,25 @@
 import React, { lazy, Suspense } from "react";
 import { Paper } from "@mui/material";
 import { useLoadDataQuery } from "../api/databaseAPI";
+import { useDataTable } from "./display-data/data-table/useDataTable";
 
-const Database = lazy(
-  () => import("./display-data/DataGridTable/FullFeaturedCrudGrid")
-);
+const Database = lazy(() => import("./display-data/data-table/DBTable"));
 
 const AppDatabase = () => {
   const { data, isLoading: loading } = useLoadDataQuery();
+  const { isCreate, isEdit, inputFields, ...handlers } = useDataTable();
   return (
     <Suspense>
       <Paper>
-        <Database data={data || []} loading={loading} />
+        <Database
+          data={data || []}
+          loading={loading}
+          oneLineRowBreakpoint="lg"
+          isCreate={isCreate}
+          isEdit={isEdit}
+          inputFields={inputFields}
+          {...handlers}
+        />
       </Paper>
     </Suspense>
   );
